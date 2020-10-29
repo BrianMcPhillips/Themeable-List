804225f36d54553483e9674922355ac6de1ff502
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-28 20:42:34 -0700

    characterItem

--- src/services/lastAirBenderApi.js
+++ src/services/lastAirBenderApi.js
@@ -1,5 +1,7 @@
 export const getCharacters = async() => {
-  const res = await fetch('https://xfiles-api.herokuapp.com/api/v1/characters');
+  const res = await fetch(
+    'https://last-airbender-api.herokuapp.com/api/v1/characters'
+  );
   const json = await res.json();
   if(!res.ok) throw 'Bad API call unable to fetch';
   
