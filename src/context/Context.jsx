af8caa32e242c17b24e206ef93237c8827b730c7
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-28 22:00:54 -0700

    context

---
+++ src/context/Context.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+export const Context = React.createContext();
+
+export const Provider = ({ childElements }) => {
+  const [dkColor, setDkColor] = useState('#545E75');
+  const [ltColor, setLtColor] = useState('#F7F7FF');
+  
+  const handleDarkColor = () => {
+    if(dkColor === '#F7F7FF')
+      setDkColor('#545E75');
+    if(ltColor === '#545E75')
+      setLtColor('F7F7FF');
+  };
+  return (
+    <Context.Provider value ={{ dkColor, ltColor, handleDarkColor }}>
+      {childElements}
+    </Context.Provider>
+  );
+};
