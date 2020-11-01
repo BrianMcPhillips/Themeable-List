82af45c1fa75784d34fb3d8012241a87d393e65d
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-31 18:45:36 -0700

    pagination started CharacterList page added

--- src/components/Characters/CharacterItem.test.jsx
+++ src/components/Characters/CharacterItem.test.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import CharacterItem from './CharacterItem';
+import { ColorProvider } from '../../context/ColorContext';
 
 describe('CharacterItem component', () => {
   it('should render a Character to the page', () => {
-    const { asFragment } = render(<CharacterItem 
-      name="Test name"
-      image="http://www.placecage.com/600/600"
-    />);
+    const { asFragment } = render(
+      <ColorProvider>
+        <CharacterItem 
+          name="Test name"
+          image="http://www.placecage.com/600/600"
+        />
+      </ColorProvider>);
 
     expect(asFragment()).toMatchSnapshot();
   });
--- src/services/lastAirBenderApi.js
+++ src/services/lastAirBenderApi.js
@@ -1,6 +1,6 @@
-export const getCharacters = async() => {
+export const getCharacters = async(page = 1) => {
   const res = await fetch(
-    'https://last-airbender-api.herokuapp.com/api/v1/characters?'
+    `https://last-airbender-api.herokuapp.com/api/v1/characters?page=${page}`
   );
   const json = await res.json();
   if(!res.ok) throw 'Bad API call unable to fetch';
